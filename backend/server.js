395c680e47f10865e7d149bc84a273514ff0c51e
Darren Roberts <dwroberts@darrens-mbp.lan>
2022-11-21 12:14:45 +0000

    Push to Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const port = process.env.PORT || 5050
-const connectDB = require('./config/db')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5050;
+const connectDB = require("./config/db");
+const path = require("path");
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(port, () => console.log(`Server started on ${port}`))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    )
+  );
+}
+
+app.listen(port, () => console.log(`Server started on ${port}`));
